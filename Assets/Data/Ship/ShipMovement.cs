283ff7a0df8a36c927c1b3ed015c111b15f0d69b
QuangDev108 <166927406+QuangDev108@users.noreply.github.com>
2024-04-17 11:06:46 +0700

    Day5

--- Assets/Data/Ship/ShipMovement.cs
+++ Assets/Data/Ship/ShipMovement.cs
@@ -4,35 +4,34 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
-    [SerializeField] protected Vector3 targetPosition;
-    [SerializeField] protected float speed = 0.05f;
 
-     void FixedUpdate()
+    [SerializeField] protected Vector3 targetPosition;
+    [SerializeField] protected float speed = 0.01f;
+
+    void FixedUpdate()
     {
         this.GetTargetPosition();
-        this.Moving();
         this.LootAtTarget();
+        this.Moving();
+    }
+
+    protected virtual void GetTargetPosition()
+    {
+        this.targetPosition = InputManager.Instance.MouseWorldPos;
+        this.targetPosition.z = 0;
     }
 
     protected virtual void LootAtTarget()
     {
         Vector3 diff = this.targetPosition - transform.parent.position;
         diff.Normalize();
-
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.parent.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
-    }    
+        transform.parent.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
 
-    protected virtual void GetTargetPosition()
-    {
-        this.targetPosition = InputManager.Instance.MouseworldPos;
-        this.targetPosition.z = 0;
-    }    
     protected virtual void Moving()
     {
         Vector3 newPos = Vector3.Lerp(transform.parent.position, targetPosition, this.speed);
         transform.parent.position = newPos;
-    }    
-
-
+    }
 }
